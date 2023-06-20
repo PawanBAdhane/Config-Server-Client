799d15a1c20e5039d9b68423333a4f706004e37f
PawanBAdhane <pawanadhane.dev@gmail.com>
2023-06-21 00:02:47 +0530

    Initial commit

---
+++ src/main/java/com/configuration/ConfigServerClient/Controller/WelcomeRestController.java
@@ -0,0 +1,18 @@
+package com.configuration.ConfigServerClient.Controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class WelcomeRestController {
+    @Value("${msg: Config Server Not Working}")
+    private String msg;
+
+    @GetMapping("/")
+    public String welcomMessage() {
+    return msg;
+    }
+}
